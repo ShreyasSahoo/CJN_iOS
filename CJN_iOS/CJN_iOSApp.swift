1ea6d66be0acc6bc20fd2c7997c8db5d8e0d9262
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-05-21 16:25:34 +0530

    Initial Commit

---
+++ CJN_iOS/CJN_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CJN_iOSApp.swift
+//  CJN_iOS
+//
+//  Created by Shreyas Sahoo on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CJN_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
